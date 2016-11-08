86012ee716da76a2fb0865374caed67a871fc9c6
Ryan Tse <rytse@mbhs.edu>
2016-11-08 00:02:38 -0500

    Make OIMap non-abstract (UNTESTED)

--- src/main/java/org/usfirst/frc/team449/robot/oi/OIMap.java
+++ src/main/java/org/usfirst/frc/team449/robot/oi/OIMap.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team449.robot.oi;
 import org.json.JSONObject;
 import org.usfirst.frc.team449.robot.RobotMap;
 
-public abstract class OIMap extends RobotMap {
+public class OIMap extends RobotMap {
     public OIMap(JSONObject json) {
         super(json);
     }
