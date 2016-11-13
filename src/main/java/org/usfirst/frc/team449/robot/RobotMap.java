d80f5bd91e096c09372b096fe749ba2a4d76de5e
Noah.Gleason <noah.gleason1@gmail.com>
2016-11-13 11:49:15 -0500

    Added a CanTalonSRX map.

--- src/main/java/org/usfirst/frc/team449/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc/team449/robot/RobotMap.java
@@ -465,6 +465,48 @@ public abstract class RobotMap {
 				+ path);
 	}
 
+	public static class CANTalonSRXMap extends RobotMap
+			.MapObject {
+		public int port;
+		public int feedbackDevice;
+		public boolean reverseSensor;
+		public boolean reverseOutput;
+		public boolean isInverted;
+		public double nominalOutVoltage;
+		public double peakOutVoltage;
+		public int profile;
+		public double kP;
+		public double kI;
+		public double kD;
+		public double kF;
+		public boolean fwdLimNormOpen;
+		public boolean revLimNormOpen;
+		public boolean fwdLimEnabled;
+		public boolean revLimEnabled;
+		public boolean fwdSoftLimEnabled;
+		public double fwdSoftLimVal;
+		public boolean revSoftLimEnabled;
+		public double revSoftLimVal;
+
+		/**
+		 * This creates a Map based on the
+		 * <code>JSONObject</code> given to it
+		 * and a path down to this object
+		 *
+		 * @param json      the <code>JSONObject</code>
+		 *                  containing the values for this
+		 *                  object
+		 * @param objPath   the path to find this object
+		 *                  in the
+		 *                  <code>JSONObject</code>
+		 * @param enclosing <code>Class</code> one up
+		 *                  from this <code>MapObject</code>
+		 */
+		public CANTalonSRXMap(JSONObject json, String objPath, Class enclosing) {
+			super(json, objPath, enclosing);
+		}
+	}
+
 	/**
 	 * Gets a boolean from the path or any <code>static</code> occurrences of the value
 	 * mentioned. <code>static</code> occurrences are defined to be any paths that are a
