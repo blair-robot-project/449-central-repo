55ca3db7f8656c535e88eaf386d3ef2f88583b38
Noah Gleason <noah.gleason1@gmail.com>
2017-12-22 17:24:32 -0500

    merge in changes from riab

--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/MappedAnalogInput.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/MappedAnalogInput.java
@@ -5,13 +5,15 @@ import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import edu.wpi.first.wpilibj.AnalogInput;
+import org.jetbrains.annotations.NotNull;
+import org.usfirst.frc.team449.robot.generalInterfaces.loggable.Loggable;
 import org.usfirst.frc.team449.robot.generalInterfaces.updatable.Updatable;
 
 /**
  * A Jackson-friendly wrapper on WPILib's {@link AnalogInput}.
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
-public class MappedAnalogInput extends AnalogInput implements Updatable {
+public class MappedAnalogInput extends AnalogInput implements Updatable, Loggable{
 
     /**
      * The value of analog input, as a percent.
@@ -52,5 +54,42 @@ public class MappedAnalogInput extends AnalogInput implements Updatable {
     @Override
     public void update() {
         percentValueCached = getPercentValue();
+    }
+
+    /**
+     * Get the headers for the data this subsystem logs every loop.
+     *
+     * @return An N-length array of String labels for data, where N is the length of the Object[] returned by getData().
+     */
+    @NotNull
+    @Override
+    public String[] getHeader() {
+        return new String[]{
+                "value"
+        };
+    }
+
+    /**
+     * Get the data this subsystem logs every loop.
+     *
+     * @return An N-length array of Objects, where N is the number of labels given by getHeader.
+     */
+    @NotNull
+    @Override
+    public Object[] getData() {
+        return new Object[]{
+                getPercentValueCached()
+        };
+    }
+
+    /**
+     * Get the name of this object.
+     *
+     * @return A string that will identify this object in the log file.
+     */
+    @NotNull
+    @Override
+    public String getName() {
+        return "Analog_"+this.getChannel();
     }
 }
--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/MappedDigitalInput.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/jacksonWrappers/MappedDigitalInput.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team449.robot.jacksonWrappers;
 import com.fasterxml.jackson.annotation.*;
 import edu.wpi.first.wpilibj.DigitalInput;
 import org.jetbrains.annotations.NotNull;
+import org.usfirst.frc.team449.robot.generalInterfaces.loggable.Loggable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
  * A series of roboRIO digital input pins.
  */
 @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
-public class MappedDigitalInput {
+public class MappedDigitalInput implements Loggable{
 
     /**
      * The digitalInputs this class represents
@@ -52,5 +53,42 @@ public class MappedDigitalInput {
             digitalValues.add(!digitalInput.get());
         }
         return digitalValues;
+    }
+
+    /**
+     * Get the headers for the data this subsystem logs every loop.
+     *
+     * @return An N-length array of String labels for data, where N is the length of the Object[] returned by getData().
+     */
+    @NotNull
+    @Override
+    public String[] getHeader() {
+        String[] toRet = new String[digitalInputs.size()];
+        for (int i = 0; i < toRet.length; i++){
+            toRet[i] = Integer.toString(digitalInputs.get(i).getChannel());
+        }
+        return toRet;
+    }
+
+    /**
+     * Get the data this subsystem logs every loop.
+     *
+     * @return An N-length array of Objects, where N is the number of labels given by getHeader.
+     */
+    @NotNull
+    @Override
+    public Object[] getData() {
+        return this.getStatus().toArray();
+    }
+
+    /**
+     * Get the name of this object.
+     *
+     * @return A string that will identify this object in the log file.
+     */
+    @NotNull
+    @Override
+    public String getName() {
+        return "DigitalInput";
     }
 }
