5828892d846d7d534d61514fc2453dbfcec26823
Natenael Kelkay <n-user@Natenaels-MacBook-Pro.local>
2017-12-28 12:56:45 -0500

    fix

--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/intake/SubsystemPosition.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/intake/SubsystemPosition.java
@@ -3,32 +3,20 @@ package org.usfirst.frc.team449.robot.subsystem.interfaces.intake;
 public interface SubsystemPosition {
 
 	// Method for setting the position to a value.
-	public static void setPosition(int value){
-
-	}
+	public void setPosition(int value);
 
 	// Method for setting the motor output to a value.
-	public static void moterOutput(int value){
-
-	}
+	public void setMoterOutput(int value);
 
 	// Method for returning the state of the reverse limit switch.
-	public static boolean stateOfReverseLimitSwitch(){
-		return false;
-	}
+	public boolean getReverseLimit();
 
 	// Method for returning the state of the forward limit switch.
-	public static boolean stateOfForwardLimitSwitch(){
-		return false;
-	}
+	public boolean getForwardLimit();
 
 	// Method for enabling the motor.
-	public static void enableTheMoter(){
-
-	}
+	public void enableMotor();
 
 	// Method for disabling the motor.
-	public static void disablingTheMoter(){
-
-	}
+	public void disableMoter();
 }
