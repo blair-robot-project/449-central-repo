aa671641a8fd7eb929013935d8733c478e70ac77
Natenael Kelkay <n-user@Natenaels-MacBook-Pro.local>
2017-12-29 08:58:24 -0500

    commands

--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/Position/commands/Enable.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/Position/commands/Enable.java
@@ -1,4 +1,21 @@
 package org.usfirst.frc.team449.robot.subsystem.interfaces.Position.commands;
 
-public class Enable {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.usfirst.frc.team449.robot.jacksonWrappers.YamlCommandWrapper;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
+public class Enable extends YamlCommandWrapper {
+
+
+
+	/**
+	 * Finish immediately because this is a state-change command.
+	 *
+	 * @return true
+	 */
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
 }
