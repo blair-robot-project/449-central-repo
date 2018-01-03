db5e432ec017d124c42290365995d7fe7692e992
Noah Gleason <noah.gleason1@gmail.com>
2018-01-03 12:27:49 -0500

    Add default Robot and RobotMap classes.

--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/RobotMap.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/RobotMap.java
@@ -1,4 +1,4 @@
-package template;
+package org.usfirst.frc.team449.robot;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.usfirst.frc.team449.robot.jacksonWrappers.MappedRunnable;
 import org.usfirst.frc.team449.robot.oi.buttons.CommandButton;
+import org.usfirst.frc.team449.robot.other.DefaultCommand;
 import org.usfirst.frc.team449.robot.other.Logger;
 
 import java.util.ArrayList;
@@ -15,10 +16,10 @@ import java.util.List;
 /**
  * The Jackson-compatible object representing the entire robot.
  */
-public class GenericRobotMap {
+public class RobotMap {
 
     /**
-     * The buttons for controlling this robot.
+     * The buttons for controlling this robot. This field only exists to prevent the list from deallocating itself.
      */
     @NotNull
     private final List<CommandButton> buttons;
@@ -36,10 +37,11 @@ public class GenericRobotMap {
     private final Runnable updater;
 
     /**
-     * The command to run during autonomous. Can be null, and if it is, no command is run during autonomous.
+     * A map of subsystems to commands that sets the default command for each subsystem to its corresponding command.
+     * This field only exists to prevent the list from deallocating itself.
      */
     @Nullable
-    private final Command autoCommand;
+    private final List<DefaultCommand> defaultCommands;
 
     /**
      * The command to be run when first enabled in autonomous mode.
@@ -65,35 +67,26 @@ public class GenericRobotMap {
      * @param buttons              The buttons for controlling this robot. Can be null for an empty list.
      * @param logger               The logger for recording events and telemetry data.
      * @param updater              A runnable that updates cached variables.
-     * @param autoCommand          The command to run during autonomous. Can be null, and if it is, no command is run
-     *                             during autonomous.
+     * @param defaultCommands      The default commands for various subsystems.
      * @param autoStartupCommand   The command to be run when first enabled in autonomous mode.
      * @param teleopStartupCommand The command to be run when first enabled in teleoperated mode.
      * @param startupCommand       The command to be run when first enabled.
      */
     @JsonCreator
-    public RobotMap2017(@Nullable List<CommandButton> buttons,
-                        @NotNull @JsonProperty(required = true) Logger logger,
-                        @NotNull @JsonProperty(required = true) MappedRunnable updater,
-                        @Nullable Command autoCommand,
-                        @Nullable Command autoStartupCommand,
-                        @Nullable Command teleopStartupCommand,
-                        @Nullable Command startupCommand) {
+    public RobotMap(@Nullable List<CommandButton> buttons,
+                    @NotNull @JsonProperty(required = true) Logger logger,
+                    @NotNull @JsonProperty(required = true) MappedRunnable updater,
+                    @Nullable List<DefaultCommand> defaultCommands,
+                    @Nullable Command autoStartupCommand,
+                    @Nullable Command teleopStartupCommand,
+                    @Nullable Command startupCommand) {
         this.buttons = buttons != null ? buttons : new ArrayList<>();
         this.logger = logger;
         this.updater = updater;
-        this.autoCommand = autoCommand;
+        this.defaultCommands = defaultCommands;
         this.autoStartupCommand = autoStartupCommand;
         this.teleopStartupCommand = teleopStartupCommand;
         this.startupCommand = startupCommand;
-    }
-
-    /**
-     * @return The buttons for controlling this robot.
-     */
-    @NotNull
-    public List<CommandButton> getButtons() {
-        return buttons;
     }
 
     /**
@@ -102,14 +95,6 @@ public class GenericRobotMap {
     @NotNull
     public Logger getLogger() {
         return logger;
-    }
-
-    /**
-     * @return The command to run during autonomous. Can be null.
-     */
-    @Nullable
-    public Command getAutoCommand() {
-        return autoCommand;
     }
 
     /**
