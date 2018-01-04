3b3f08aef753115554d3b28ed165fcc779d290c1
Natenael Kelkay <n-user@Natenaels-MacBook-Pro.local>
2018-01-04 14:18:03 -0500

    fix

--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/Position/commands/PositionCommands.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/Position/commands/PositionCommands.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import org.jetbrains.annotations.NotNull;
 import org.usfirst.frc.team449.robot.jacksonWrappers.YamlCommandWrapper;
+import org.usfirst.frc.team449.robot.other.Logger;
 import org.usfirst.frc.team449.robot.subsystem.interfaces.Position.SubsystemPosition;
 import org.usfirst.frc.team449.robot.subsystem.interfaces.intake.SubsystemIntake;
 
@@ -17,11 +18,47 @@ public class PositionCommands extends YamlCommandWrapper {
 	@NotNull
 	private final SubsystemPosition subsystem;
 
+	/**
+	 * Default constructor
+	 *
+	 * @param subsystem The subsystem to execute this command on.
+	 */
 	public PositionCommands (@NotNull@JsonProperty(required = true) SubsystemPosition subsystem){
 		this.subsystem = subsystem;
 	}
+
+	/**
+	 * Log when this command is initialized
+	 */
+
+	@Override
+	protected void initialize() { Logger.addEvent("PositionCommands init.", this.getClass());}
+
+	/**
+	 * Enables the motor.
+	 */
+	@Override
+	protected void execute() {
+		subsystem.enableMotor();
+	}
+
+	/**
+	 * Finish immediately because this is a state-change command.
+	 *
+	 * @return true
+	 */
 	@Override
 	protected boolean isFinished() {
 		return false;
 	}
+
+	/**
+	 * Log when this command ends
+	 */
+	@Override
+	protected void end() {
+		Logger.addEvent("PositionCommands end.", this.getClass());
+	}
+
 }
+
