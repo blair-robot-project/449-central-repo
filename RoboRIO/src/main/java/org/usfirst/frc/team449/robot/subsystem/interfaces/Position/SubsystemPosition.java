165a8ee9f6d02dd043d1016be75fbd029ab07660
Natenael Kelkay <n-user@Natenaels-MacBook-Pro.local>
2018-01-11 17:59:43 -0500

    Finished Commands

--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/Position/SubsystemPosition.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/Position/SubsystemPosition.java
@@ -3,10 +3,10 @@ package org.usfirst.frc.team449.robot.subsystem.interfaces.Position;
 public interface SubsystemPosition {
 
 	// Method for setting the position to a value.
-	public void setPosition(int value);
+	public void setPosition(double value);
 
 	// Method for setting the motor output to a value.
-	public void setMotorOutput(int value);
+	public void setMotorOutput(double value);
 
 	// Method for returning the state of the reverse limit switch.
 	public boolean getReverseLimit();
--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/Position/commands/Home.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/subsystem/interfaces/Position/commands/Home.java
@@ -7,6 +7,18 @@ import org.jetbrains.annotations.NotNull;
 import org.usfirst.frc.team449.robot.jacksonWrappers.YamlCommandWrapper;
 import org.usfirst.frc.team449.robot.subsystem.interfaces.Position.SubsystemPosition;
 
+
+/*
+- go backwards until the reverse limit switch is pressed
+Then finish and disable command
+
+- need to pass the speed to go in as a parameter
+
+-pass whether to use the forward or reverse limit switch for homing in as a parameter
+
+-speed should be a double from 0 to 1
+ */
+
 @JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class)
 public class Home extends YamlCommandWrapper {
 
@@ -16,17 +28,46 @@ public class Home extends YamlCommandWrapper {
 	@NotNull
 	private final SubsystemPosition subsystem;
 
+	@NotNull
+	private double speed;
+
+	@NotNull
+	private boolean useForward;
+
 	/**
 	 * Default constructor
 	 *
 	 * @param subsystem The subsystem to execute this command on.
 	 */
-	public Home(@NotNull@JsonProperty(required = true) SubsystemPosition subsystem){
+	public Home(@NotNull@JsonProperty(required = true) SubsystemPosition subsystem, double speed, boolean useForward){
 		this.subsystem = subsystem;
+		this.speed = speed;
+		this.useForward = useForward;
+	}
+
+	@Override
+	protected void execute(){
+		if(useForward){
+			subsystem.setMotorOutput(speed);
+		}else
+		{
+			subsystem.setMotorOutput(-speed);
+		}
 	}
 
 	@Override
 	protected boolean isFinished() {
-		return false;
+
+		if(useForward == true){
+			return subsystem.getForwardLimit();
+		}
+		else {
+			return subsystem.getReverseLimit();
+		}
 	}
+	@Override
+	public void end(){
+		subsystem.disableMotor();
+	}
+
 }
