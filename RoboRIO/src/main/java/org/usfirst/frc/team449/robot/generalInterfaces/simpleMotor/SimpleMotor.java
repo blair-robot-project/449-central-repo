5a4347f67c4f3812706031181b33be3f9c416972
Noah Gleason <noah.gleason1@gmail.com>
2018-01-10 21:08:53 -0500

    Add some more documentation

--- RoboRIO/src/main/java/org/usfirst/frc/team449/robot/generalInterfaces/simpleMotor/SimpleMotor.java
+++ RoboRIO/src/main/java/org/usfirst/frc/team449/robot/generalInterfaces/simpleMotor/SimpleMotor.java
@@ -2,6 +2,9 @@ package org.usfirst.frc.team449.robot.generalInterfaces.simpleMotor;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+/**
+ * A motor with velocity/voltage control and the ability to enable and disable.
+ */
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.WRAPPER_OBJECT, property = "@class")
 public interface SimpleMotor {
 
