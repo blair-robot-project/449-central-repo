477b609266ae8a0feb2337887ef94833ddb3e190
Noah Gleason <noah.gleason1@gmail.com>
2018-04-15 13:37:24 -0500

    Add script for wheelbase analysis

---
+++ R scripts/wheelbaseMeasurer.R
@@ -0,0 +1,28 @@
+rad2deg <- function(rad) {return((rad * 180) / (pi))}
+deg2rad <- function(deg) {return((deg * pi) / (180))}
+
+#Smooths a value while taking its derivative with respect to time.
+smoothDerivative <- function(value, timeMillis, n){
+  smoothed <- (value[(n+1):length(value)] - value[1:(length(value)-n)])/((timeMillis[(n+1):length(timeMillis)] - timeMillis[1:(length(timeMillis)-n)])/1000);
+  return(smoothed);
+}
+
+wheelbaseVsAngVel <- function(logTime, clockTime, angularDisplacementDegrees, leftPos, rightPos, n, minDegreesPerSec){
+  deltaLeft <- smoothDerivative(leftPos, logTime, n)
+  deltaRight <- smoothDerivative(rightPos, logTime, n)
+  deltaTheta <- smoothDerivative(angularDisplacementDegrees, clockTime, n)
+  dat <- data.frame(deltaLeft = deltaLeft, deltaRight = deltaRight, deltaTheta = deltaTheta)
+  dat <- subset(dat, abs(deltaTheta) > minDegreesPerSec)
+  dat <- subset(dat, is.finite(deltaTheta))
+  dat$wheelbase <- (dat$deltaRight - dat$deltaLeft)/deg2rad(dat$deltaTheta)
+  dat$absDTheta <- abs(dat$deltaTheta)
+  model <- lm(dat$wheelbase ~ dat$deltaTheta)
+  plot(dat$deltaTheta, dat$wheelbase)
+  abline(model, col="green")
+  print(model)
+  model <- lm(dat$wheelbase ~ dat$absDTheta)
+  plot(dat$absDTheta, dat$wheelbase)
+  abline(model, col="green")
+  print(paste("Weighted average wheelbase:",weighted.mean(dat$wheelbase, dat$absDTheta)))
+  return(model)
+}
