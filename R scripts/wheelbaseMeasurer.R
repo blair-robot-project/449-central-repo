945bfb586b77fbdd9a665feb36e6ea665046b2c6
Noah Gleason <noah.gleason1@gmail.com>
2018-05-01 08:53:02 -0400

    Add oldWheelbaseVsAngVel

--- R scripts/wheelbaseMeasurer.R
+++ R scripts/wheelbaseMeasurer.R
@@ -16,10 +16,24 @@ wheelbaseVsAngVel <- function(logTime, clockTime, angularDisplacementDegrees, le
   dat <- subset(dat, is.finite(deltaTheta))
   dat$wheelbase <- (dat$deltaRight - dat$deltaLeft)/deg2rad(dat$deltaTheta)
   dat$absDTheta <- abs(dat$deltaTheta)
-  model <- lm(dat$wheelbase ~ dat$deltaTheta)
-  plot(dat$deltaTheta, dat$wheelbase)
+  model <- lm(dat$wheelbase ~ dat$absDTheta)
+  plot(dat$absDTheta, dat$wheelbase)
   abline(model, col="green")
-  print(model)
+  print(paste("Weighted average wheelbase:",weighted.mean(dat$wheelbase, dat$absDTheta)))
+  return(model)
+}
+
+oldWheelbaseVsAngVel <- function(logTime, angularVel, leftPos, rightPos, n, minDegreesPerSec){
+  deltaLeft <- smoothDerivative(leftPos, logTime, n)
+  deltaRight <- smoothDerivative(rightPos, logTime, n)
+  deltaTheta <- angularVel[ceiling(n/2+1):(length(angularVel)-floor(n/2))]
+  dat <- data.frame(deltaLeft = deltaLeft, deltaRight = deltaRight, deltaTheta = deltaTheta)
+  dat <- subset(dat, abs(deltaTheta) > minDegreesPerSec)
+  dat <- subset(dat, is.finite(deltaTheta))
+  dat <- subset(dat, is.finite(deltaLeft))
+  dat <- subset(dat, is.finite(deltaRight))
+  dat$wheelbase <- (dat$deltaRight - dat$deltaLeft)/deg2rad(dat$deltaTheta)
+  dat$absDTheta <- abs(dat$deltaTheta)
   model <- lm(dat$wheelbase ~ dat$absDTheta)
   plot(dat$absDTheta, dat$wheelbase)
   abline(model, col="green")
