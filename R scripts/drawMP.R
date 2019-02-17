b0e4e22dd33fb217d5fe4a89041274a3f7541a17
Noah Gleason <noah.gleason1@gmail.com>
2019-02-17 08:45:59 -0500

    Update DrawMP.R

--- R scripts/drawMP.R
+++ R scripts/drawMP.R
@@ -1,16 +1,37 @@
-plotProfile <- function(profileName, inverted = FALSE, wheelbaseDiameter, centerToBack, startY = 0, startPos = c(-1,-1,-1,-1,-1,-1), usePosition = TRUE){
-  left <- read.csv(paste("../robot2019Left",profileName,"Profile.csv",sep=""), header=FALSE)
-  right <- read.csv(paste("../robot2019Right",profileName,"Profile.csv",sep=""), header=FALSE)
+#Simple helper methods
+rad2deg <- function(rad) {return((rad * 180) / (pi))}
+deg2rad <- function(deg) {return((deg * pi) / (180))}
+distance <- function(x1, y1, x2, y2){return(sqrt((x1-x2)^2 + (y1-y2)^2))}
+leftKVFwd <- 1.483639
+leftKVRev <- 1.465076
+leftKA <- 0.179707
+leftInterceptFwd <- 0.718235
+leftInterceptRev <- 0.969839
+rightKVFwd <- 1.479436
+rightKVRev <- 1.439699
+rightKA <- 0.171817
+rightInterceptFwd <- 0.703256
+rightInterceptRev <- 0.984755
+
+
+plotProfile <- function(profileName, leftInverted = FALSE, rightInverted=FALSE, wheelbaseDiameter, centerToBack, startY = 0, startPos = c(-1,-1,-1,-1,-1,-1), usePosition = TRUE){
+  if (leftInverted & rightInverted){
+    left <- read.csv(paste("../../449-central-repo/naviRight",profileName,"Profile.csv",sep=""), header=FALSE)
+    right <- read.csv(paste("../../449-central-repo/naviLeft",profileName,"Profile.csv",sep=""), header=FALSE)
+  } else {
+    left <- read.csv(paste("../../449-central-repo/naviLeft",profileName,"Profile.csv",sep=""), header=FALSE)
+    right <- read.csv(paste("../../449-central-repo/naviRight",profileName,"Profile.csv",sep=""), header=FALSE)
+  }
   startingCenter <- c(startY, centerToBack)
   left$V1[1] <- 0
   left$V2[1] <- 0
-  left$V3[1] <- left$V3[2]
+  left$V4[1] <- left$V4[2]
   right$V1[1] <- 0
   right$V2[1] <- 0
-  right$V3[1] <- right$V3[2]
-  #Position,Velocity,Delta t, Elapsed time
-  left$V4 <- (0:(length(left$V1)-1))*left$V3[1]
-  right$V4 <- (0:(length(right$V1)-1))*right$V3[1]
+  right$V4[1] <- right$V4[2]
+  #Position,Velocity,Accel,Delta t, Elapsed time
+  left$V5 <- (0:(length(left$V1)-1))*left$V4[1]
+  right$V5 <- (0:(length(right$V1)-1))*right$V4[1]
   #Time, Left X, Left Y, Right X, Right Y, Angle
   out <- array(dim=c(length(left$V1),6))
   if(identical(startPos, c(-1,-1,-1,-1,-1,-1))){
@@ -19,27 +40,47 @@ plotProfile <- function(profileName, inverted = FALSE, wheelbaseDiameter, center
     out[1,]<-startPos
   }
   
-  for(i in 2:length(left$V4)){
+  for(i in 2:length(left$V5)){
     #Get the angle the robot is facing.
     perpendicular <- out[i-1,6]
-    print(perpendicular)
     
     #Add the change in time
-    out[i,1] <- out[i-1,1]+left$V3[i]
+    out[i,1] <- out[i-1,1]+left$V4[i]
     
     #Figure out linear change for each side using position or velocity
     if (usePosition){
       deltaLeft <- left$V1[i] - left$V1[i-1]
       deltaRight <- right$V1[i] - right$V1[i-1]
     } else {
-      deltaLeft <- left$V2[i]*left$V3[i]
-      deltaRight <- right$V2[i]*left$V3[i]
+      deltaLeft <- left$V2[i]*left$V4[i]
+      deltaRight <- right$V2[i]*left$V4[i]
     }
     
     # Invert the change if nessecary
-    if (inverted){
+    if (leftInverted){
+      # print(paste("L:",left$V2[i]*leftKVRev+left$V3[i]*leftKA+leftInterceptRev))
+      if (left$V2[i]*leftKVRev+left$V3[i]*leftKA+leftInterceptRev > 12){
+        print("Over 12v!")
+      }
       deltaLeft <- -deltaLeft
+    } else {
+      # print(paste("L:",left$V2[i]*leftKVFwd+left$V3[i]*leftKA+leftInterceptFwd))
+      if (left$V2[i]*leftKVFwd+left$V3[i]*leftKA+leftInterceptFwd > 12){
+        print("Over 12v!")
+      }
+    }
+    
+    if (rightInverted){
+      # print(paste("R:",right$V2[i]*rightKVRev+right$V3[i]*rightKA+rightInterceptRev))
+      if (right$V2[i]*rightKVRev+right$V3[i]*rightKA+rightInterceptRev > 12){
+        print("Over 12v!")
+      }
       deltaRight <- -deltaRight
+    } else {
+      # print(paste("R:",right$V2[i]*rightKVFwd+right$V3[i]*rightKA+rightInterceptFwd))
+      if (right$V2[i]*rightKVFwd+right$V3[i]*rightKA+rightInterceptFwd > 12){
+        print("Over 12v!")
+      }
     }
     
     diffTerm <- deltaRight - deltaLeft
@@ -73,7 +114,7 @@ plotProfile <- function(profileName, inverted = FALSE, wheelbaseDiameter, center
       #The is the length of the vector pointing towards the new position of each
       #wheel divided by the radius of the turning circle.
       vectorDistanceWithoutR <- 2*sin(theta/2)
-    
+      
       out[i, 2] <- out[i-1,2]+vectorDistanceWithoutR*leftR*cos(vectorTheta)
       out[i, 3] <- out[i-1,3]+vectorDistanceWithoutR*leftR*sin(vectorTheta)
       out[i, 4] <- out[i-1,4]+vectorDistanceWithoutR*rightR*cos(vectorTheta)
@@ -90,7 +131,7 @@ drawProfile <- function (coords, centerToBack, wheelbaseDiameter, clear=TRUE, li
     } else {
       plot(coords[,2],coords[,3], col="Green", ylim=c(-16, 16), xlim = c(0,54), xlab = "X Position (feet)", ylab="Y Position (feet)", asp=1)
     }
-    plotField("deepSpaceField.csv")
+    plotField("powerUpField.csv")
   } else {
     if (linePlot){
       lines(coords[,2],coords[,3],col="Green")
@@ -140,6 +181,47 @@ plotField <- function(filename, xOffset=0, yOffset=0){
   for (i in 1:length(field$x1)){
     lines(c(field$x1[i]+xOffset, field$x2[i]+xOffset), c(field$y1[i]+yOffset, field$y2[i]+yOffset), col=field$col[i])
   }
+}
+
+tracedAnimation <- function(x, y, leftX, leftY, rightX, rightY, headingRadians, deltaTime, fieldFile, robotFile, robotRadius, frameSize=-1, filename="animation.mp4", robotCircleFile=NA){
+  library("animation")
+  theta <- headingRadians
+  saveVideo({
+    for(i in 1:length(x)){
+      #Set up frame
+      if(frameSize == -1){
+        plot(x=c(),y=c(),xlim=c(min(x)-robotRadius, max(x)+robotRadius),ylim=c(min(y)-robotRadius, max(y)+robotRadius), asp=1, xlab="X position (feet)", ylab="Y position (feet)")
+      } else {
+        plot(x=c(),y=c(),xlim=c(x[i]-frameSize/2, x[i]+frameSize/2),ylim=c(y[i]-frameSize/2, y[i]+frameSize/2),asp=1, xlab="X position (feet)", ylab="Y position (feet)")
+      }
+      lines(x=leftX[1:i], y=leftY[1:i], col="Green")
+      lines(x=rightX[1:i], y=rightY[1:i], col="Red")
+      plotField(fieldFile, 0, 0)
+      drawRobot(robotFile, x[i],y[i],theta[i],robotCircleFile = robotCircleFile)
+    }
+  }, interval = deltaTime, ani.width = 1920, ani.height = 1080, video.name=filename)
+}
+
+executeProfileSequence <- function(names, leftInverted, rightInverted, wheelbaseDiameter, centerToBack, startY, robotFile, intakeFile = NA){
+  out <- plotProfile(names[1], leftInverted = leftInverted[1], rightInverted = rightInverted[1], wheelbaseDiameter=wheelbaseDiameter, centerToBack=centerToBack, startY = startY,  usePosition = TRUE)
+  totalOut <- out
+  drawProfile(out, centerToBack = centerToBack, wheelbaseDiameter = wheelbaseDiameter, clear = TRUE, linePlot = TRUE)
+  tmp <- out[length(out[,1]),]
+  drawRobot(robotFile, x=(tmp[2]+tmp[4])/2, y= (tmp[3]+tmp[5])/2, theta = tmp[6], intakeFile)
+  if(length(names) > 1){
+    for(i in 2:length(names)){
+      print(paste("Starting profile",i))
+      out <- plotProfile(names[i], leftInverted = leftInverted[i], rightInverted = rightInverted[i], wheelbaseDiameter=wheelbaseDiameter, centerToBack=centerToBack, startPos = tmp,  usePosition = TRUE)
+      totalOut <- rbind(totalOut, out)
+      drawProfile(out, centerToBack = centerToBack, wheelbaseDiameter = wheelbaseDiameter, clear = FALSE, linePlot = TRUE)
+      tmp <- out[length(out[,1]),]
+      drawRobot(robotFile, x=(tmp[2]+tmp[4])/2, y= (tmp[3]+tmp[5])/2, theta = tmp[6], intakeFile)
+    }
+  }
+  #Time, Left X, Left Y, Right X, Right Y, Angle
+  print(paste("X:",(tmp[2]+tmp[4])/2,"Y:",(tmp[3]+tmp[5])/2))
+  print(rad2deg(tmp[6]))
+  return(totalOut)
 }
 
 wheelbaseDiameter <- 25.5/12.
